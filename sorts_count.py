7c665c7255fe6ed2e09af5263ba7f3b072b115c7
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2023-02-01 20:43:38 -0800

    7th commit

--- sorts_count.py
+++ sorts_count.py
@@ -38,9 +38,9 @@ def insertion_count(a_list):
 
     value = a_list[index] #item in list at each index
     pos = index - 1 #comparing value to each item to the left in the list
-    while pos >= 0:
-      comparisons +=1
-      if a_list[pos] > value: #this line needs to be broken per instructions
+    while pos >= 0: #separated from "if a_list[pos] > value:" so comparisons aren't ran on on comparing the index to zero
+      comparisons +=1 #checking for comparisons in values of the list
+      if a_list[pos] > value: #compares values in the list
           exchanges += 1 #checking for exchanges in the list
           a_list[pos + 1] = a_list[pos]
           pos -= 1
